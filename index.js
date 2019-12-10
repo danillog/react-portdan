af1f2f59a35936dd3ddfb2131dae2c5a8892fa96
danillog <danillog821@gmail.com>
2019-12-10 15:30:47 -0400

    2 files modified

--- index.js
+++ index.js
@@ -15,26 +15,21 @@ class App extends Component {
     return (
       <div class="container-fluid">
         <div class="row">
-          <div class="col-lg-12 agua">
+          <div class="col-lg agua">
             <Wave />
           </div>
-          <div class="row">
-            <div class="col-12 caixa">
-              <div class="row">
-                <div class="col">
+          <div class="row ">
+            <div class="col-sm caixa">            
+                <div>
                   <h1> {this.state.name}</h1>
-                  <p>Start editing to see some magic happen aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa:)</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed placerat eros. Fusce vel sem purus. Nullam eget eleifend nunc. Cras imperdiet finibus pellentesque. Suspendisse varius felis in vestibulum elementum. Mauris tortor libero, efficitur eget quam et, lobortis lobortis elit. Sed interdum euismod varius. Sed nec congue massa, non egestas urna. Aenean volutpat dui eget nisi gravida, ac efficitur nisi convallis. Maecenas efficitur finibus erat eu fringilla. Donec consectetur in enim at dapibus. Fusce facilisis euismod interdum. Donec elementum enim neque, et sollicitudin elit hendrerit ac. Fusce ac consectetur sapien.)</p>
                   <h1> {this.state.name}</h1>
-                  <p>Start editing to see some magic happen aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa:)</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed placerat eros. Fusce vel sem purus. Nullam eget eleifend nunc. Cras imperdiet finibus pellentesque. Suspendisse varius felis in vestibulum elementum. Mauris tortor libero, efficitur eget quam et, lobortis lobortis elit. Sed interdum euismod varius. Sed nec congue massa, non egestas urna. Aenean volutpat dui eget nisi gravida, ac efficitur nisi convallis. Maecenas efficitur finibus erat eu fringilla. Donec consectetur in enim at dapibus. Fusce facilisis euismod interdum. Donec elementum enim neque, et sollicitudin elit hendrerit ac. Fusce ac consectetur sapien.)</p>
                   <h1> {this.state.name}</h1>
-                  <p>Start editing to see some magic happen aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa:)</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed placerat eros. Fusce vel sem purus. Nullam eget eleifend nunc. Cras imperdiet finibus pellentesque. Suspendisse varius felis in vestibulum elementum. Mauris tortor libero, efficitur eget quam et, lobortis lobortis elit. Sed interdum euismod varius. Sed nec congue massa, non egestas urna. Aenean volutpat dui eget nisi gravida, ac efficitur nisi convallis. Maecenas efficitur finibus erat eu fringilla. Donec consectetur in enim at dapibus. Fusce facilisis euismod interdum. Donec elementum enim neque, et sollicitudin elit hendrerit ac. Fusce ac consectetur sapien.)</p>
                   <h1> {this.state.name}</h1>
-                  <p>Start editing to see some magic happen aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa:)</p>
-                  <h1> {this.state.name}</h1>
-                  <p>Start editing to see some magic happen aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa:)</p>
-
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed placerat eros. Fusce vel sem purus. Nullam eget eleifend nunc. Cras imperdiet finibus pellentesque. Suspendisse varius felis in vestibulum elementum. Mauris tortor libero, efficitur eget quam et, lobortis lobortis elit. Sed interdum euismod varius. Sed nec congue massa, non egestas urna. Aenean volutpat dui eget nisi gravida, ac efficitur nisi convallis. Maecenas efficitur finibus erat eu fringilla. Donec consectetur in enim at dapibus. Fusce facilisis euismod interdum. Donec elementum enim neque, et sollicitudin elit hendrerit ac. Fusce ac consectetur sapien.)</p>
                 </div>
-              </div>
             </div>
           </div>
         </div>
