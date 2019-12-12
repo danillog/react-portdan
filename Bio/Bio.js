b606e7a880111963826e64bea42c190a1f777857
danillog <danillog821@gmail.com>
2019-12-11 21:51:14 -0400

    3 files modified

--- Bio/Bio.js
+++ Bio/Bio.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react';
-import './Bio.css';
+import React, { Component } from "react";
+import "./Bio.css";
 
 class Bio extends Component {
   render() {
     return (
-    <div class="frame">
-      <h2>Ola mundo</h2>
-    </div>
-      
+      <div className="row">
+        <div className="col-sm-4">
+          <div className="photo">
+            <img src="http://github.com/danillog.png" />
+            <div className="glass" />
+          </div>
+        </div>
+        <div className="col-sm-8 text">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+            sed placerat eros. Fusce vel sem purus. Nullam eget eleifend nunc.
+            Cras imperdiet finibus pellentesque. Suspendisse varius felis in
+            vestibulum elementum. Mauris tortor libero, efficitur eget quam et,
+            lobortis lobortis elit. Sed interdum euismod varius. Sed nec congue
+            massa, non egestas urna. Aenean volutpat dui eget nisi gravida, ac
+            efficitur nisi convallis. Maecenas efficitur finibus erat eu
+            fringilla. Donec consectetur in enim at dapibus. Fusce facilisis
+            euismod interdum. Donec elementum enim neque, et sollicitudin elit
+            hendrerit ac. Fusce ac consectetur sapien.)
+          </p>
+        </div>
+      </div>
     );
   }
 }
--- index.js
+++ index.js
@@ -23,7 +23,7 @@ class App extends Component {
             <div class="col-sm caixa">            
                 <div>
                   <Bio />
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed placerat eros. Fusce vel sem purus. Nullam eget eleifend nunc. Cras imperdiet finibus pellentesque. Suspendisse varius felis in vestibulum elementum. Mauris tortor libero, efficitur eget quam et, lobortis lobortis elit. Sed interdum euismod varius. Sed nec congue massa, non egestas urna. Aenean volutpat dui eget nisi gravida, ac efficitur nisi convallis. Maecenas efficitur finibus erat eu fringilla. Donec consectetur in enim at dapibus. Fusce facilisis euismod interdum. Donec elementum enim neque, et sollicitudin elit hendrerit ac. Fusce ac consectetur sapien.)</p>
+                 
                   <h1> {this.state.name}</h1>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed placerat eros. Fusce vel sem purus. Nullam eget eleifend nunc. Cras imperdiet finibus pellentesque. Suspendisse varius felis in vestibulum elementum. Mauris tortor libero, efficitur eget quam et, lobortis lobortis elit. Sed interdum euismod varius. Sed nec congue massa, non egestas urna. Aenean volutpat dui eget nisi gravida, ac efficitur nisi convallis. Maecenas efficitur finibus erat eu fringilla. Donec consectetur in enim at dapibus. Fusce facilisis euismod interdum. Donec elementum enim neque, et sollicitudin elit hendrerit ac. Fusce ac consectetur sapien.)</p>
                   <h1> {this.state.name}</h1>
