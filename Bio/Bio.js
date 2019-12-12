8689bd9fcb5af12041fc45b3d5c0ad46980bcd28
danillog <danillog821@gmail.com>
2019-12-11 21:02:48 -0400

    2 files modified, 2 added, 1 deleted

---
+++ Bio/Bio.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Bio.css';
+
+class Bio extends Component {
+  render() {
+    return (
+    <div class="frame">
+      <h2>Ola mundo</h2>
+    </div>
+      
+    );
+  }
+}
+
+export default Bio;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import "./index.css";
 import Wave from "./Wave/Wave";
+import Bio from "./Bio/Bio";
 
 class App extends Component {
   constructor() {
@@ -21,7 +22,7 @@ class App extends Component {
           <div class="row ">
             <div class="col-sm caixa">            
                 <div>
-                  <h1> {this.state.name}</h1>
+                  <Bio />
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed placerat eros. Fusce vel sem purus. Nullam eget eleifend nunc. Cras imperdiet finibus pellentesque. Suspendisse varius felis in vestibulum elementum. Mauris tortor libero, efficitur eget quam et, lobortis lobortis elit. Sed interdum euismod varius. Sed nec congue massa, non egestas urna. Aenean volutpat dui eget nisi gravida, ac efficitur nisi convallis. Maecenas efficitur finibus erat eu fringilla. Donec consectetur in enim at dapibus. Fusce facilisis euismod interdum. Donec elementum enim neque, et sollicitudin elit hendrerit ac. Fusce ac consectetur sapien.)</p>
                   <h1> {this.state.name}</h1>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed placerat eros. Fusce vel sem purus. Nullam eget eleifend nunc. Cras imperdiet finibus pellentesque. Suspendisse varius felis in vestibulum elementum. Mauris tortor libero, efficitur eget quam et, lobortis lobortis elit. Sed interdum euismod varius. Sed nec congue massa, non egestas urna. Aenean volutpat dui eget nisi gravida, ac efficitur nisi convallis. Maecenas efficitur finibus erat eu fringilla. Donec consectetur in enim at dapibus. Fusce facilisis euismod interdum. Donec elementum enim neque, et sollicitudin elit hendrerit ac. Fusce ac consectetur sapien.)</p>
